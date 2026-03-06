1dfd377e4f5e821dbdda825a72127c17537ca392
RITESH KUMAR <ritesharya9955@gmail.com>
2026-03-07 01:26:21 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,6 +42,7 @@ const Navbar = () => {
                 </div>
                 <Link to='/Cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading--4 bg-black text-white aspect-square rounded-full text-[8px]'></p>
                 </Link>
             </div>
 
