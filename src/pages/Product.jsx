735ab5125c47f9e3149cd4380a7b0eb26f19c184
Ritesh Kumar <ritesharya9955@gmail.com>
2026-03-18 01:32:36 +0530

    description and review

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,9 +6,10 @@ import { assets, products } from '../assets/assets';
 const Product = () => {
 
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image,setImage] = useState('')
+  const [size,setSize] = useState('')
 
 
   const fetchProductData = async () => {
@@ -56,13 +57,42 @@ const Product = () => {
           <img src={assets.star_icon} alt="" className="w-3 5" />
           <img src={assets.star_icon} alt="" className="w-3 5" />
           <img src={assets.star_dull_icon} alt="" className="w-3 5" />
-          <p>
+          <p className='pl-2'>(122) </p>
+
+        </div>
+        <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+        <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+        <div className=' flex flex-col gap-4 my-8'>
+          <p>Select Size</p>
+          <div className='flex gap-2'>
             
-          </p>
+            {productData.sizes.map((item,index)=>(
+              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item=== size ? 'border-orange-500' :  ''} `} key={index}>{item}</button>
+            ))}
+
+          </div>
+
+        </div>
+        <button className='bg-black text-white px-8 py-3 text-sm active:bggray-700'>ADD TO CART</button>
+        <hr className='mt-8 sm:w-4/5' />
+        <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+          <p>100% Original product. </p>
+          <p>Cash on delivery is available on this product.</p>
+          <p>Easy return and exchange policy within 7 days</p>
 
         </div>
 
+
       </div>
+      </div>
+      {/* ---------- Description and review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+
+        </div>
+
       </div>
     </div>
   ): <div className='opcity-0'></div>
