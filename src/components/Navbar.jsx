6d15be7c2966352cb7f623a460e18f8a2765528b
Ritesh Kumar <ritesharya9955@gmail.com>
2026-03-19 00:41:12 +0530

    cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     const navigate = useNavigate();
 
-    const {setShowSearch} = useContext(ShopContext)
+    const {setShowSearch, getCartCount} = useContext(ShopContext)
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
@@ -48,7 +48,7 @@ const Navbar = () => {
                 </div>
                 <Link to='/Cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>12</p>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
                 </Link>
                 <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
             </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -36,14 +36,31 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
 
   }
+  const getCartCount = () => {
+    let totalCount = 0;
+    for(const items in cartItems){
+      for(const item in cartItems [items]){
+        try {
+          if(cartItems[items][item] > 0){
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalCount;
+  }
+   
 
-  
+  const  updateQuantity = async ()
  
   const value = {
 
     products, currency, delivery_fee,
     search,setSearch,showSearch,setShowSearch,
-    cartItems,addToCard
+    cartItems,addToCard,
+    getCartCount
   }
   return (
     <ShopContext.Provider value={value}>
